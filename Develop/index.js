8fc54a622536cdbad162fad925deef8eb1672add
Nick Morris <nicholasmorris2599@gmail.com>
2022-12-22 09:08:59 -0800

    Added questions with types, messages, and names.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,54 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: "What is your project's name?",
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT License', 'Apachie License 2.0', 'ISC License', 'GNU PGLv2']
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'dependencies'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repository?',
+        name: 'note'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contribution'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
