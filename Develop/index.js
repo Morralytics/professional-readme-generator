9058a04f08ff1d4f7f3c295aaf09725454595907
Nick Morris <nicholasmorris2599@gmail.com>
2023-01-06 15:37:37 -0800

    Installed inquirer.

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         message: 'What is your GitHub username?',
-        name: 'email'
+        name: 'git'
     },
     {
         type: 'input',
